18f6e1c1a1a1785150c8c5ed94c32dab63f7a213
Chris Calderon <calderon.christian760@gmail.com>
2016-07-08 15:13:03 -0700

    Fixed bug in setup.py that broke when trying to install a dep from a github url.

--- setup.py
+++ setup.py
@@ -1,7 +1,25 @@
 from setuptools import setup
 
+
+def is_http(req):
+    """Checks if a requirement is a link."""
+    return req.startswith('http://') or req.startswith('https://')
+
+
+def dep_sorter(rs, r):
+    """Appends the requirement r to the proper list."""
+    print rs, r
+    if is_http(r):
+        rs[1].append(r)
+    else:
+        rs[0].append(r)
+    return rs
+
 with open('requirements.txt') as reqs:
-    requirements = filter(None, [r.strip() for r in reqs])
+    requirements, links = reduce(dep_sorter,
+                                 filter(None,
+                                        [r.strip() for r in reqs]),
+                                 [[],[]])
 
 setup(name='contract',
       version='1.0dev1',
@@ -9,4 +27,5 @@ setup(name='contract',
       author='ChrisCalderon',
       author_email='pythonwiz@protonmail.com',
       packages=['contract'],
-      install_requires=requirements)
+      install_requires=requirements,
+      dependency_links=links)
